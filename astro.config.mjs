80e8e1f70201532038297dab7f85fa42ed165af8
Administrator <admin@example.com>
2026-02-02 17:00:20 +0700

    ci: deploy to cpanel via ftp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 const isProd = process.env.NODE_ENV === 'production';
-const base = isProd ? '/hrpro' : '/';
-const site = 'https://ddos-spec.github.io/hrpro';
+const base = isProd ? '/github' : '/';
+const site = 'https://hrproduction.id/github';
 
 // https://astro.build/config
 export default defineConfig({

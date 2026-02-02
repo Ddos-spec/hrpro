753cc7e8e92ffcb2ddbec10c9fab3983599e9964
Administrator <admin@example.com>
2026-02-02 12:15:26 +0700

    chore: configure gh-pages output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+const base = isProd ? '/hrpro' : '/';
+const site = 'https://ddos-spec.github.io/hrpro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	outDir: './docs',
+	site,
+	base,
+});
